3438010b6e35ef6d19fbab417b22cb6c61ce9723
Petr Malanik <Malanik.Petr.gm@gmail.com>
2025-03-05 15:27:23 +0100

    scripts/update_rpi: restart avahi afterwards, adds support for hostname

--- scripts/update_rpi.sh
+++ scripts/update_rpi.sh
@@ -1,53 +1,34 @@
 #!/bin/bash
 
-#parameter is ip address of rpi
-if [[ $1 =~ ^[0-9]+\.[0-9]+\.[0-9]+\.[0-9]+$ ]]; then
-    # Input is already an IP address
-    IP_ADDRESS=$1
-else
-    # Input is a hostname, try to resolve it
-    RESOLVED_IP=$(getent hosts "$1" | awk '{ print $1 }')
-
-    if [ -z "$RESOLVED_IP" ]; then
-        # Try specific mDNS resolution if getent doesn't work
-        RESOLVED_IP=$(avahi-resolve -n -4 "$1" 2>/dev/null | awk '{ print $2 }')
-    fi
-
-    if [ -z "$RESOLVED_IP" ]; then
-        echo "Error: Could not resolve hostname $1"
-        exit 1
-    fi
-
-    echo "Resolved $1 to $RESOLVED_IP"
-    IP_ADDRESS=$RESOLVED_IP
-fi
-
-#if ip address is empty print help
-if [ -z "$IP_ADDRESS" ]; then
-  echo "Usage: $0 <ip_address>"
+if [ -z "$1" ]; then
+  echo "Usage: $0 <hostname_or_ip>"
   exit 1
 fi
 
-echo "IP_ADDRESS: $IP_ADDRESS"
-
-# Set general reactor key if accessed via mDNS
-SSH_OPTS=""
-if [ "$USING_RESOLVED_IP" = true ]; then
-    SSH_OPTS="-i ~/.ssh/reactor"
-    echo "Using reactor SSH key for authentication"
+#parameter is ip address of rpi
+if [[ $1 =~ ^[0-9]+\.[0-9]+\.[0-9]+\.[0-9]+$ ]]; then
+    # Input is already an IP address
+    HOST="root@$1"
+else
+    HOST="$1"
 fi
+
+echo "Host: $HOST"
 
 #run build_rpi.sh
 ./scripts/build_rpi.sh
 
 #stop services over ssh
-ssh root@${IP_ADDRESS} systemctl stop avahi-daemon.socket avahi-daemon.service can0.service reactor-database-export.service reactor-core-module.service reactor-api-server.service reactor-web-control.service
+ssh ${HOST} systemctl stop can0.service reactor-database-export.service reactor-core-module.service reactor-api-server.service reactor-web-control.service
 
 #rsync to rpi
-rsync -rtv --no-perms $SSH_OPTS \build/rpi/install/filesystem/ root@${IP_ADDRESS}:/
+rsync -rv --no-perms \build/rpi/install/filesystem/ ${HOST}:/
 
 #update systemd services
-ssh root@${IP_ADDRESS} systemctl daemon-reload
+ssh ${HOST} systemctl daemon-reload
 
 #start services over ssh
-ssh root@${IP_ADDRESS} systemctl start avahi-daemon.socket avahi-daemon.service can0.service reactor-database-export.service reactor-core-module.service reactor-api-server.service reactor-web-control.service
+ssh ${HOST} systemctl start can0.service reactor-database-export.service reactor-core-module.service reactor-api-server.service reactor-web-control.service
+
+#restart avahi separately at he end to avoid issue not non existing hostname
+ssh ${HOST} systemctl restart avahi-daemon.socket avahi-daemon.service
